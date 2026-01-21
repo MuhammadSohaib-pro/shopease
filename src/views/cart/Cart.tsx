8a287f7057d2ee9718b8c34d065da5d197c2207b
Muhammad Sohaib <93024641+MuhammadSohaib-pro@users.noreply.github.com>
2026-01-22 01:30:03 +0500

    feat: resolved build issue

--- src/views/cart/Cart.tsx
+++ src/views/cart/Cart.tsx
@@ -1,6 +1,6 @@
 import ProductTile from "@/components/product/ProductTile";
 import { Button } from "@/components/ui/button";
-import { ArrowRight, ShoppingBag } from "lucide-react";
+import { ArrowRight } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const Cart = () => {
